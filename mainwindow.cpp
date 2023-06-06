7871c50fdf4e929cf045d640b2de219334bd4b7d
GrimmyReaps <tomek1545@interia.pl>
2023-06-06 16:04:27 +0200

    WorkingTable
    ToDo: Function that fill the table from JSON

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,10 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
+#include <QDebug>
 #include <QCoreApplication>
+#include <QTableWidget>
+#include <QTableWidgetItem>
+#include <QString>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -8,6 +12,17 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui->setupUi(this);
     connect(ui->quitButton, SIGNAL(clicked()), this, SLOT(close()));
+
+    QTableWidget *passwords = ui->passwordsShower;
+    passwords->horizontalHeader()->setStretchLastSection(true);
+    passwords->insertColumn(0);
+    passwords->insertColumn(1);
+    QStringList passwordlabel = {"Strony", "Hasła"};
+    passwords->setHorizontalHeaderLabels(passwordlabel);
+    passwords->insertRow(passwords->rowCount());
+    int currentRow = passwords->rowCount() - 1;
+
+    passwords->setItem(currentRow, 0, new QTableWidgetItem("wordpress"));
 }
 
 MainWindow::~MainWindow()
