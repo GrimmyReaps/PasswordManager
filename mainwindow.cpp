20ba36667682707619040ec0798b53979bf534d7
GrimmyReaps <tomek1545@interia.pl>
2023-06-07 00:26:08 +0200

    JSON reading achieved
    Now we need to show what we read

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -2,9 +2,13 @@
 #include "./ui_mainwindow.h"
 #include <QDebug>
 #include <QCoreApplication>
-#include <QTableWidget>
 #include <QTableWidgetItem>
 #include <QString>
+#include <QFile>
+#include <QByteArray>
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QJsonArray>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -13,16 +17,11 @@ MainWindow::MainWindow(QWidget *parent)
     ui->setupUi(this);
     connect(ui->quitButton, SIGNAL(clicked()), this, SLOT(close()));
 
-    QTableWidget *passwords = ui->passwordsShower;
-    passwords->horizontalHeader()->setStretchLastSection(true);
-    passwords->insertColumn(0);
-    passwords->insertColumn(1);
-    QStringList passwordlabel = {"Strony", "Hasła"};
-    passwords->setHorizontalHeaderLabels(passwordlabel);
-    passwords->insertRow(passwords->rowCount());
-    int currentRow = passwords->rowCount() - 1;
+    //Place for Array that will hold all info
 
-    passwords->setItem(currentRow, 0, new QTableWidgetItem("wordpress"));
+    QTableWidget *passwords = ui->passwordsShower;
+    initialSetup(passwords);
+    readJson();
 }
 
 MainWindow::~MainWindow()
@@ -30,3 +29,37 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+//Table initialization function
+void MainWindow::initialSetup(QTableWidget *widget){
+    widget->horizontalHeader()->setStretchLastSection(true);
+    widget->insertColumn(0);
+    widget->insertColumn(1);
+    QStringList passwordlabel = {"Strony", "Hasła"};
+    widget->setHorizontalHeaderLabels(passwordlabel);
+    //widget->insertRow(widget->rowCount());
+    //int currentRow = widget->rowCount() - 1;
+
+    //widget->setItem(currentRow, 0, new QTableWidgetItem("wordpress"));
+}
+
+//reads JSON file
+//TODO add JSON info to that fucking 2D array please
+void MainWindow::readJson(){
+    QFile jsonFile("C:\\Users\\tomek\\Desktop\\PW\\Projekt\\PasswordManager\\Passwords.json");
+    jsonFile.open(QIODevice::ReadOnly | QIODevice::Text);
+    QByteArray jsonFileData = jsonFile.readAll();
+    jsonFile.close();
+
+    QJsonDocument jsonDocument = QJsonDocument::fromJson(jsonFileData);
+    QJsonObject jsonObject = jsonDocument.object();
+
+    QJsonValue pairsArrayValue = jsonObject.value("pairsArray");
+    QJsonArray pairsArray = pairsArrayValue.toArray();
+
+    foreach(const QJsonValue & v, pairsArray){
+        qDebug() << v.toObject().value("site").toString();
+        qDebug() << v.toObject().value("password").toString();
+
+        qDebug() << "---------------------------";
+    }
+}
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,8 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QTableWidget>
+
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -17,5 +19,7 @@ public:
 
 private:
     Ui::MainWindow *ui;
+    void initialSetup(QTableWidget *widget);
+    void readJson();
 };
 #endif // MAINWINDOW_H
